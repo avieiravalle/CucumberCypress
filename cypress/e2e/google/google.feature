967eb9b909c27fc3f3bfd19a9dbb35be74484399
Anderson Vieira <avieiravale@gmail.com>
2023-06-14 12:32:06 -0300

    pastas

---
+++ cypress/e2e/google/google.feature
@@ -0,0 +1,7 @@
+Feature: google
+
+Scenario: Search
+
+Given busca
+When submeter
+Then resultado
---
+++ cypress/e2e/google/google.js
@@ -0,0 +1,13 @@
+import {Given, When, Then  } from "cypress-cucumber-preprocessor";
+
+Given('busca',()=>{
+    cy.visit('https://www.google.com/')
+})
+
+When('submeter',()=>{
+
+})
+
+Then('resultado', ()=>{
+
+})
